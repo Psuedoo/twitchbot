65e7c88c084c410eaa5336af3160e0b8c8f6a8d3
Psuedoo <43244534+Psuedoo@users.noreply.github.com>
2020-10-25 00:23:46 -0400

    fixed sound integrations

--- commands_class.py
+++ commands_class.py
@@ -1,11 +1,13 @@
 import os
 from config import Config
 from tinydb import TinyDB, Query
+from pathlib import Path
 
 class Command:
     def __init__(self, channel):
         self.config = Config(channel)
-        self.db = TinyDB('commands/commands_{self.config.channel_name}.json')
+        self.db_path = Path.cwd() / 'commands' / f'commands_{self.config.channel_name}.json'
+        self.db = TinyDB(self.db_path)
 
     def add_command(self, command_name, command_response):
         # Adds command to db  
